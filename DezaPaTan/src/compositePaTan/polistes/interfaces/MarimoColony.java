98d84bd9924e406a6a76d4c1ae2722a784ed8ac4
polistes <USER@alicewhite>
2011-04-06 00:48:09 +0900

    マージしたらおかしなことに…

--- DezaPaTan/src/compositePaTan/polistes/interfaces/MarimoColony.java
+++
@@ -1,8 +0,0 @@
-package compositePaTan.polistes.interfaces;
-
-public interface MarimoColony {
-	public abstract String getName();
-	public abstract int getSize();
-	public abstract void add(MarimoColony mc);
-	public abstract String printList(String prefix);
-}
